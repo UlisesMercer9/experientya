45c54eb14373a4c70f0a177398d9b96854cdd600
Ulises Rodríguez <ulisesmercer9@gmail.com>
2022-06-04 23:53:35 -0500

    feat: first commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Navbar/>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/home/Navbar.vue'
+
+export default {
+    components: {
+        Navbar
+    }
+}
+</script>
